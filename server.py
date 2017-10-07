338e1dedd7a73a35f7d4a861d956c4d24cb25217
Mikol Aspinwall <maaspinwall@goshen.edu>
2017-10-06 23:39:58 -0400

    Encoding
    Python 3.x doesn't handle strings the same way 2.x does

--- server.py
+++ server.py
@@ -11,5 +11,5 @@ s.listen(5)
 while True:
     c, addr = s.accept()
     print("Got connection from", addr)
-    c.send("Thank you for connecting")
+    c.send(bytes("Thank you for connecting", "UTF-8"))
     c.close()
