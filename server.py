eabce8e03bae8eeadbe33b708a7236f8b89ee7bf
mi-kol <mikol.aspinwall@gmail.com>
2017-10-07 06:01:05 +0000

    consistency of utf encoding for packet insecurity

--- server.py
+++ server.py
@@ -5,10 +5,11 @@ import socket
 s = socket.socket()
 port = 12345
 s.bind(('', port))
+print "Now listening."
 
 s.listen(5)
 while True:
     c, addr = s.accept()
     print("Got connection from", addr)
-    c.send(bytes("Thank you for connecting", "UTF-8"))
+    c.send("Thank you for connecting")
     c.close()
