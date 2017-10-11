038075ac76cd075a2e0040ca0f69239f6efb16ba
Mikol Aspinwall <maaspinwall@goshen.edu>
2017-10-11 18:02:24 -0400

    multichat server and client
    and the beginnings of the markov server!

---
+++ markovserver.py
@@ -0,0 +1,15 @@
+import pykov
+import socket
+import select
+
+# networking variables
+host = ''
+socket_list = []
+recv_buffer = 4096
+port = 12345
+
+#markov variables
+transition_matrix = 
+
+
+def markov_server()
